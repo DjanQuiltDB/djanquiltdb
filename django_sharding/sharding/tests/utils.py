66a709910a0a6dd899579c68ee42c559cf9a0f2d
Sebastiaan ten Pas <sebastiaan.tenpas@patchman.co>
2017-08-16 17:08:47 +0200

    Changed Shard.STATE_ACTIVE to State.ACTIVE

--- django_sharding/sharding/tests/utils.py
+++ django_sharding/sharding/tests/utils.py
@@ -539,7 +539,7 @@ class ForEachShardTestCase(TestCase):
 
         with mock.patch('sharding.utils.create_schema_on_node'):
             self.shard1 = Shard.objects.create(alias='test_sharding', schema_name='test_schema', node_name='default',
-                                               state=Shard.STATE_ACTIVE)
+                                               state=State.ACTIVE)
 
     def repeatable_function(self, shard=None, shard_id=None, **kwargs):
         if shard:
