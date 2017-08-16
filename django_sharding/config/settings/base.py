5647fc5a2cef8c5444e3b211d4adb2e7c133918d
Daniel ter Horst <daniel.terhorst@patchman.co>
2017-08-16 14:43:25 +0200

    Add middleware that allows wrapping all views with the `use_shard` context manager.

--- django_sharding/config/settings/base.py
+++ django_sharding/config/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'example.middleware.UseShardMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- django_sharding/sharding/middleware.py
+++ django_sharding/sharding/middleware.py
@@ -1,8 +1,9 @@
+import django
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils.module_loading import import_string
 
-from sharding.utils import StateException
+from sharding.utils import StateException, use_shard, get_shard_class
 
 
 class StateExceptionMiddleware(object):
@@ -15,3 +16,59 @@ class StateExceptionMiddleware(object):
             response = HttpResponse()
             response.status_code = 503
             return response
+
+
+class BaseUseShardMiddleware(object):
+    shard_context_manager = None
+
+    def get_shard_id(self, request):
+        raise NotImplementedError(
+            'The `BaseUseShardMiddleware` middleware class requires that `get_shard_id` is implemented.'
+        )
+
+    def process_view(self, request, *args, **kwargs):
+        shard_id = self.get_shard_id(request)
+
+        if shard_id:
+            shard = get_shard_class().objects.get(id=shard_id)
+
+            self.shard_context_manager = use_shard(shard)
+            self.shard_context_manager.__enter__()
+
+    def process_exception(self, request, exception):
+        if self.shard_context_manager:
+            self.shard_context_manager.__exit__(
+                exc_type=exception.__class__,
+                exc_value=exception,
+                exc_traceback=exception.__traceback__,
+            )
+
+    def process_response(self, request, response):
+        if self.shard_context_manager:
+            self.shard_context_manager.__exit__(None, None, None)
+
+        return response
+
+# With Django 1.10 we can use the "MIDDLEWARE" setting and a new
+# middleware format to write use the context manager without
+# manually calling enter and exit.
+if django.VERSION[:1] >= (1, 10):
+    class BaseUseShardMiddleware(object):
+        def __init__(self, get_response):
+            self.get_response = get_response
+
+        def __call__(self, request):
+            shard_id = self.get_shard_id(request)
+
+            if shard_id:
+                shard = get_shard_class().objects.get(id=shard_id)
+
+                with use_shard(shard):
+                    return self.get_response(request)
+
+            return self.get_response(request)
+
+        def get_shard_id(self, request):
+            raise NotImplementedError(
+                'The `BaseUseShardMiddleware` middleware class requires that `get_shard_id` is implemented.'
+            )
