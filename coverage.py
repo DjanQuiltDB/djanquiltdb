93be7c1c00685ae86a6b89871ee81f4602706ced
Dmitrii Prihodco <dmitrii.prihodco@patchman.co>
2023-11-14 12:27:06 +0100

    SHARDING-136 Fix coverage report

--- coverage.py
+++ coverage.py
@@ -9,7 +9,7 @@ from functools import reduce
 status_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'coverage', 'status.json')
 
 # Read and sum the code coverage results from the HTML's status.json
-_, statements, missing, _, _, partial, _ = list(
+_, _, statements, missing, _, _, partial, _ = list(
     reduce(
         lambda x, y: [j + y[i] for i, j in enumerate(x)],
         [z['index']['nums'] for z in json.load(open(status_file))['files'].values()]
